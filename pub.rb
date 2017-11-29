f7837ccb3f6129c12bc83f2420a9ed7d88322a94
Robert Henderson <robert@nterpryz.com>
2017-11-29 11:10:18 +0000

    Tested Pub Has Name, Till & Drinks

--- pub.rb
+++ pub.rb
@@ -2,10 +2,10 @@ class Pub
 
   attr_reader :name, :till, :drinks
 
-  def initialize(name, till, drinks)
-      @name = name
-      @till = till
-      @drinks = drinks
+  def initialize(input_name, input_till, input_drinks)
+      @name = input_name
+      @till = input_till
+      @drinks = input_drinks
   end
 
 
--- specs/pub_specs.rb
+++ specs/pub_specs.rb
@@ -1,12 +1,32 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../pub.rb')
+require_relative('../drink.rb')
 
 
 class TestPub < Minitest::Test
 
   def setup
-
+  @drink1 = Drink.new("Lager", 5)
+  @drink2 = Drink.new("Vodka", 10)
+  @drink3 = Drink.new("Jager", 1)
+  @chanter_drinks = [@drink1, @drink2, @drink3]
+  @chanter_till = 200
+  @pub = Pub.new("Chanter", @chanter_till, @chanter_drinks)
   end
+
+
+  def test_pub_has_name
+    assert_equal("Chanter", @pub.name)
+  end
+
+  def test_pub_has_till
+    assert_equal(200, @pub.till)
+  end
+
+  def test_pub_has_drinks
+    assert_equal(3, @pub.drinks.count)
+  end
+
 
 end
